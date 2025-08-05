a9fd4ef5d7d9bf5dffd0e20413f1415108bee555
cjiho0312 <cjiho0312@naver.com>
2025-08-05 15:40:15 +0900

    8.5

---
+++ Class 4th (Select Sort)/source.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+#pragma region 선택 정렬
+	// 주어진 리스트 중에 최소값을 찾은 다음 특정한 위치에서
+	// 결과를 교체하는 방식으로 정렬하는 알고리즘입니다.
+
+	int arr[] = { 4, 6, 8, 3, 1, 1, 9, 7, 1, 2 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+
+	for (int i = 0; i < size - 1; i++)
+	{
+		int min = arr[i];
+		int minIndex;
+
+		for (int j = 0 + i; j < size; j++)
+		{
+			if (arr[j] < min)
+			{
+				min = arr[j];
+				minIndex = j;
+			}
+		}
+
+		if (min != arr[i])
+		{
+			swap(arr[i], arr[minIndex]);
+
+			// int temp = min;
+			// arr[minIndex] = arr[i];
+			// arr[i] = temp;
+		}
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+
+#pragma endregion
+
+	return 0;
+}
---
+++ Class 5th (Insert Sort)/source.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+#pragma region 삽입 정렬
+	// 데이터를 하나씩 확인하면서 이미 정렬된 부분과 비교하여
+	// 자신의 위치를 찾아 삽입하는 방식으로 정렬하는 알고리즘입니다.
+
+	int list[] = { 8, 2, 4, 1, 10, 7, 3, 9, 5, 6 };
+	int size = sizeof(list) / sizeof(list[0]);
+
+
+	for (int i = 1; i < size; i++)
+	{
+		int key = list[i];
+
+		for (int j = i - 1; j >= 0; j--)
+		{
+			if (key < list[j])
+			{
+				list[j + 1] = list[j];
+				list[j] = key;
+			}
+		}
+	}
+
+	for (const auto& element : list)
+	{
+		cout << element << " ";
+	}
+
+
+#pragma endregion
+
+
+
+	return 0;
+}
