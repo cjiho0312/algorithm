4e2ec16f704f7efec6a90ba8be979d494fd25cbd
cjiho0312 <cjiho0312@naver.com>
2025-08-06 15:43:45 +0900

    8.6

---
+++ Class 6th (Euclidean Algorithm)/source.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+
+int function(int x, int y)
+{
+	// 1. 두 개의 자연수 x, y가 있을 때
+	// x와 y를 나눈 나머지를 r이라고 합니다.
+
+	// 2. r이 0이라면, x와 y의 최대공약수가 y가 됩니다.
+
+	// 3. r이 0이 아니라면, x와 y의 최대 공약수는 y와 r의
+	// 최대 공약수와 같기 때문에 x에는 y 그리고 y에는 r을 대입한 후
+	// 1번으로 돌아가면 됩니다.
+
+	int r = x % y;
+
+	while (r != 0)
+	{
+		r = x % y;
+
+		if (r != 0)
+		{
+			x = y;
+			y = r;
+		}
+	}
+
+	r = y;
+
+	return r;
+
+}
+
+
+int main()
+{
+#pragma region 유클리드 호제법
+	// 자연수 또는 정수의 최대 공약수를 구하는 방법으로
+	// 두 수가 서로 상대방 수를 나누어서 원하는 수를 얻어내는 알고리즘입니다.
+
+
+	cout << "최대 공약수 : " << function(270, 192);;
+
+
+
+#pragma endregion
+
+
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,9 +1,59 @@
 ﻿#include <iostream>
-#include <algorithm>
+#include <cmath>
 using namespace std;
+
+
+void sieve(int n)
+{
+
+
+	int* arr = new int[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = i + 1;
+
+		if (i == 0)
+		{
+			arr[i] = 0;
+		}
+		else
+		{
+			for (int j = 2; j < sqrt(n); j++)
+			{
+				if (arr[i] == j)
+				{
+					break;
+				}
+				if (arr[i] % j == 0)
+				{
+					arr[i] = 0;
+				}
+			}
+		}
+	}
+	
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] != 0)
+		{
+			cout << arr[i] << " ";
+		}
+	}
+
+	delete[] arr;
+}
 
 int main()
 {
+#pragma region 에라토스테네스의 체
+
+	sieve(50);
+
+#pragma endregion
+
+
+
 
 	return 0;
 }
