d687981747a6a46252c6e48bceb278e78a61b8b0
cjiho0312 <cjiho0312@naver.com>
2025-08-08 14:02:18 +0900

    8.8

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,73 +2,46 @@
 
 using namespace std;
 
-void find(int list[], int size, int findKey) // 반복문 활용
-{
-	int firstIndex = 0;
-	int endIndex = size - 1;
-	int pivotIndex = (firstIndex + endIndex) / 2;
-
-	while (list[firstIndex] <= list[endIndex])
-	{
-		if (findKey == list[pivotIndex])
-		{
-			cout << "찾았습니다! " << findKey << "은(는) list[" << pivotIndex << "]에 있습니다." << endl;
-			return;
-		}
-		else if (findKey < list[pivotIndex])
-		{
-			endIndex = pivotIndex - 1;
-			pivotIndex = endIndex;
-		}
-		else if (findKey > list[pivotIndex])
-		{
-			firstIndex = pivotIndex + 1;
-			pivotIndex = firstIndex;
-		}
-	}
-
-	cout << "list에서 " << findKey << "을(를) 찾을 수 없습니다." << endl;
-
-}
-
-void find_(int list[], int findKey, int firstIndex, int endIndex) // 재귀함수 활용
-{
-	if (list[firstIndex] > list[endIndex] || findKey > list[endIndex] || findKey < list[firstIndex])
-	{
-		cout << "list에서 " << findKey << "을(를) 찾을 수 없습니다." << endl;
-		return;
-	}
-	else
-	{
-		int pivotIndex = (firstIndex + endIndex) / 2;
-
-		if (findKey == list[pivotIndex])
-		{
-			cout << "찾았습니다! " << findKey << "은(는) list[" << pivotIndex << "]에 있습니다." << endl;
-			return;
-		}
-		else if (findKey < list[pivotIndex])
-		{
-			find_(list, findKey, firstIndex, pivotIndex - 1);
-		}
-		else if (findKey > list[pivotIndex])
-		{
-			find_(list, findKey, pivotIndex + 1, endIndex);
-		}
-	}
-}
-
 int main()
 {
-#pragma region 이분 탐색
-	// 탐색 범위를 반으로 나누어 찾는 값을 포함하는 범위를
-	// 좁혀 나가는 방식으로 작동하는 알고리즘입니다.
+#pragma region 계수정렬
+	// 데이터의 값을 비교하지 않고 각 원소에 데이터가 몇 개 있는지
+	// 개수를 세어 저장한 다음 정렬하는 알고리즘입니다.
 
-	int list[] = { 6, 10, 13, 22, 57 };
+	int list[] = { 1,3,2,2,4,5,1,2 };
 	int size = sizeof(list) / sizeof(list[0]);
+	int maxSize = 5;
 
-	// find(list, size, 58);
-	find_(list, 58, 0, size - 1);
+	int *arr = new int[maxSize];
+
+	for (int i = 0; i < maxSize; i++)
+	{
+		arr[i] = 0;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j <= maxSize; j++)
+		{
+			if (list[i] == j + 1)
+			{
+				arr[j]++;
+				break;
+			}
+		}
+	}
+
+	int index = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < arr[i]; j++)
+		{
+			list[index] = i + 1;
+			cout << list[index] << " ";
+			index++;
+		}
+	}
 
 #pragma endregion
 
