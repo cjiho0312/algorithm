a0dd1aa03db3a989cec24aacc97b379157de722e
cjiho0312 <cjiho0312@naver.com>
2025-08-21 15:45:47 +0900

    8.21 합병 정렬

---
+++ Class 19th (Krusckal)/source.cpp
@@ -0,0 +1,147 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define SIZE 8
+
+class Krusckal
+{
+private:
+
+	int cost;
+	int parent[SIZE];
+
+	class Edge
+	{
+	private:
+		int x, y, w;
+
+	public:
+		Edge(int vertexX, int vertexY, int weight)
+		{
+			this->x = vertexX;
+			this->y = vertexY;
+			this->w = weight;
+		}
+
+		const int& X() { return x; }
+		const int& Y() { return y; }
+		const int& W() { return w; }
+
+		const bool& operator <(const Edge& other)
+		{
+			return this->w < other.w;
+		}
+	};
+
+	vector<Edge> nodelist;
+
+public:
+
+	Krusckal()
+	{
+		cost = 0;
+
+		for (int i = 0; i < SIZE; i++)
+		{
+			parent[i] = i;
+		}
+	}
+
+	void insert(int vertexX, int vertexY, int weight)
+	{
+		Edge edge(vertexX, vertexY, weight);
+
+		nodelist.push_back(edge);
+	}
+
+	void calculate()
+	{
+		sort(nodelist.begin(), nodelist.end());
+	}
+
+	int find(int x)
+	{
+		if (x == parent[x])
+		{
+			return x;
+		}
+		else
+		{
+			return parent[x] = find(parent[x]);
+		}
+	}
+
+	void Union(int x, int y)
+	{
+		int a = find(x);
+		int b = find(y);
+
+		if (a == b) return;
+
+		if (a < b)
+		{
+			parent[y] = a;
+		}
+		else
+		{
+			parent[x] = b;
+		}
+	}
+
+	bool same(int x, int y)
+	{
+		return find(x) == find(y);
+	}
+
+	void runKrusckal()
+	{
+		for (int i = 0; i < nodelist.size(); i++)
+		{
+			if (!same(nodelist[i].X(), nodelist[i].Y()))
+			{
+				Union(nodelist[i].X(), nodelist[i].Y());
+
+				cost += nodelist[i].W();
+			}
+		}
+
+		cout << "Min cost : " << cost << endl;
+	}
+};
+
+
+int main()
+{
+#pragma region 최소 신장 트리
+	// 그래프의 모든 정점을 포함하면서 사이클이 존재하지 않는
+	// 부분 그래프로, 그래프의 모든 정점을 최소 비용으로 연결하는 트리입니다.
+
+	// 그래프의 정점의 수가 n개일 때, 간선의 수는 n-1개 입니다.
+
+	Krusckal kruskal;
+
+	kruskal.insert(1, 7, 12);
+	kruskal.insert(4, 7, 13);
+	kruskal.insert(4, 1, 30);
+	kruskal.insert(5, 7, 79);
+	kruskal.insert(4, 2, 23);
+	kruskal.insert(1, 2, 71);
+	kruskal.insert(2, 5, 65);
+	kruskal.insert(1, 5, 15);
+	kruskal.insert(6, 5, 44);
+	kruskal.insert(3, 5, 18);
+	kruskal.insert(3, 6, 36);
+
+	kruskal.calculate();
+
+	kruskal.runKrusckal();
+
+#pragma endregion
+
+
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -4,142 +4,150 @@
 
 using namespace std;
 
-#define SIZE 8
+#define INFINITY 10000000
+#define SIZE 6
 
-class Kruckal
+class Dijkstra
 {
 private:
+	int graph[SIZE][SIZE] { 0 };
 
-	int cost = 0;
-	int parent[SIZE];
-
-	class Edge
-	{
-	private:
-		int x, y, w;
-
-	public:
-		Edge(int vertexX, int vertexY, int weight)
-		{
-			this->x = vertexX;
-			this->y = vertexY;
-			this->w = weight;
-		}
-
-		const int& X() { return x; }
-		const int& Y() { return y; }
-		const int& W() { return w; }
-
-		const bool & operator <(const Edge&other)
-		{
-			return this->w < other.w;
-		}
-	};
-	
-	vector<Edge> nodelist;
+	int distance[SIZE];
+	bool visited[SIZE];
 
 public:
-
-	void insert(int vertexX, int vertexY, int weight)
-	{
-		Edge edge(vertexX, vertexY, weight);
-
-		nodelist.push_back(edge);
-	}
-
-	void calculate()
-	{
-		sort(nodelist.begin(), nodelist.end());
-	}
-
-	int find(int x)
-	{
-		if (x == parent[x])
-		{
-			return x;
-		}
-		else
-		{
-			return parent[x] = find(parent[x]);
-		}
-	}
-
-	void Union(int x, int y)
-	{
-		int a = find(x);
-		int b = find(y);
-
-		if (a == b) return;
-
-		if (a < b)
-		{
-			parent[y] = a;
-		}
-		else
-		{
-			parent[x] = b;
-		}
-	}
-
-	bool same(int x, int y)
-	{
-		return find(x) == find(y);
-	}
-
-	void runKruckal()
+	Dijkstra()
 	{
 		for (int i = 0; i < SIZE; i++)
 		{
-			parent[i] = i;
+			visited[i] = false;
+			distance[i] = 0;
 		}
+	}
 
-		for (int i = 0; i < nodelist.size(); i++)
+	void insert(int x, int y, int w)
+	{
+		graph[x - 1][y - 1] = w;
+		graph[y - 1][x - 1] = w;
+	}
+
+	const int& find() // 방문 체크한 요소 제외 최소값 찾고, 인덱스값 반환
+	{
+		int min = INFINITY;
+		int index;
+
+		for (int i = 0; i < SIZE; i++)
 		{
-			if (!same(nodelist[i].X(), nodelist[i].Y()))
+			if (visited[i]==false && distance[i] != 0 && min > distance[i])
 			{
-				Union(nodelist[i].X(), nodelist[i].Y());
-
-				cost += nodelist[i].W();
+				min = distance[i];
+				index = i;
 			}
 		}
 
-		cout << "min cost : " << cost << endl;
+		return index;
+	}
+
+	void update(int start) // 경로 중 더 작은 값의 경로가 나오면 distance 갱신
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			distance[i] = graph[start][i];
+		}
+
+		visited[start] = true;
+
+		for (int i = 0; i < SIZE - 1; i++)
+		{
+			int minNode = find();
+
+			visited[minNode] = true;
+
+			for (int j = 0; j < SIZE; j++)
+			{
+				if (visited[j] == false)
+				{
+					if (distance[minNode] + graph[minNode][j] < distance[j])
+					{
+						distance[j] = distance[minNode] + graph[minNode][j];
+					}
+				}
+			}
+		}
+
+		for (const auto& element : distance)
+		{
+			cout << element << " ";
+		}
+
+	}
+
+	void print()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			cout << "[" << i << "] ";
+
+			for (int j = 0; j < SIZE; j++)
+			{
+				cout << graph[i][j] << " ";
+			}
+			
+			cout << endl;
+		}
 	}
 };
 
-
 int main()
 {
-#pragma region 최소 신장 트리
-	// 그래프의 모든 정점을 포함하면서 사이클이 존재하지 않는
-	// 부분 그래프로, 그래프의 모든 정점을 최소 비용으로 연결하는 트리입니다.
+#pragma region 다익스트라 알고리즘
+	// 시작점으로부터 모든 노드까지의 최소 거리를 구해주는
+	// 알고리즘입니다.
 
-	// 그래프의 정점의 수가 n개일 때, 간선의 수는 n-1개 입니다.
+	// 1. 거리 배열에서 weight[시작 노드]의 값들로 초기화합니다.
 
-	Kruckal kruskal;
+	// 2. 시작점을 방문 처리합니다.
 
-	kruskal.insert(1, 7, 12);
-	kruskal.insert(4, 7, 13);
-	kruskal.insert(4, 1, 30);
-	kruskal.insert(5, 7, 79);
-	kruskal.insert(4, 2, 23);
-	kruskal.insert(1, 2, 71);
-	kruskal.insert(2, 5, 65);
-	kruskal.insert(1, 5, 15);
-	kruskal.insert(6, 5, 44);
-	kruskal.insert(3, 5, 18);
-	kruskal.insert(3, 6, 36);
+	// 3. 거리 배열에서 최소 비용 노드를 찾고 방문 처리합니다.
+	//    단, 이미 방문한 노드는 제외합니다.
 
-	kruskal.calculate();
+	// 4. 최소 비용 노드를 거쳐갈 지 고민해서 거리 배열을 갱신합니다.
+	//	  단, 이미 방문한 노드는 제외합니다.
 
-	kruskal.runKruckal();
+	// 5. 모든 노드를 방문할 때까지 3 ~ 4번을 반복합니다.
 
+	// 방문하지 않은 노드 중에서 가장 작은 거리를 가진 노드를
+	// 방문하고, 그 노드롸 연결된 다른 노드까지의 거리를 계산합니다.
+
+	Dijkstra dijkstra;
+
+	dijkstra.insert(1, 2, 2);
+	dijkstra.insert(1, 3, 5);
+	dijkstra.insert(1, 4, 1);
+	dijkstra.insert(1, 5, INFINITY);
+	dijkstra.insert(1, 6, INFINITY);
+
+	dijkstra.insert(2, 3, 3);
+	dijkstra.insert(2, 4, 2);
+	dijkstra.insert(2, 5, INFINITY);
+	dijkstra.insert(2, 6, INFINITY);
+
+	dijkstra.insert(3, 4, 3);
+	dijkstra.insert(3, 5, 1);
+	dijkstra.insert(3, 6, 5);
+
+	dijkstra.insert(4, 5, 1);
+	dijkstra.insert(4, 6, INFINITY);
+
+	dijkstra.insert(5, 6, 2);
+
+	// dijkstra.print();
+
+	dijkstra.update(0);
 
 
 
 #pragma endregion
-
-
 
 	return 0;
 }
